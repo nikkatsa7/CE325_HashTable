7272ec5f1c0c0ccc6b9171228e3cb06a45581b63
nikkatsa7 <nikkatsa@inf.uth.gr>
2017-09-08 19:30:19 +0300

    Fully working HashTable and extended RehashTable

---
+++ Katsaros_Nikolaos_1648/ExtensibleHashTable.h
@@ -0,0 +1,150 @@
+
+#ifndef __EXTENSIBLE_HASHTABLE_H__
+#define __EXTENSIBLE_HASHTABLE_H__
+
+#include "HashTable.h"
+
+#include <string>
+using namespace std;
+
+class ExtensibleHashTable: public HashTable {
+private:
+  double upper_bound_ratio, lower_bound_ratio;
+  void rehash();
+  bool hitUpper();
+  bool hitLower();
+
+public:
+  ExtensibleHashTable(double upper_bound_ratio=0.5,
+                      double lower_bound_ratio=0.125,
+                      int size=8);
+  ExtensibleHashTable(const ExtensibleHashTable &t);
+  bool add(const string &str);
+  bool remove(const string &str);
+  bool operator << (string str);
+  bool operator >> (string str);
+  ExtensibleHashTable operator+(const ExtensibleHashTable &t) const;
+  ExtensibleHashTable &operator+=(const ExtensibleHashTable &t);
+  ExtensibleHashTable &operator=(const ExtensibleHashTable &t);
+
+};
+
+ExtensibleHashTable::ExtensibleHashTable(double upper_bound_ratio,double lower_bound_ratio,int size):
+  HashTable(size),upper_bound_ratio(upper_bound_ratio),lower_bound_ratio(lower_bound_ratio){}
+
+ExtensibleHashTable::ExtensibleHashTable(const ExtensibleHashTable &t):
+HashTable(t),upper_bound_ratio(upper_bound_ratio),lower_bound_ratio(lower_bound_ratio){}
+
+bool ExtensibleHashTable::hitUpper(){
+  return(((double(this->size)/double(this->capacity)))>this->upper_bound_ratio);
+}
+
+bool ExtensibleHashTable::hitLower(){
+  return(((double(this->size)/double(this->capacity)))<this->lower_bound_ratio);
+}
+
+void ExtensibleHashTable::rehash(){
+  ExtensibleHashTable rht;
+  int newCapacity;
+
+  if(hitUpper()){
+    newCapacity = this->capacity*2;
+
+  }else if(hitLower()){
+    newCapacity = this->capacity/2;
+
+  } else{
+    return;
+  }
+
+  rht.capacity = newCapacity;
+  rht.table = new string [rht.capacity];
+  rht.size = 0;
+
+  for(int i=0; i<this->capacity; i++){
+    rht.HashTable::add(this->table[i]);
+  }
+
+  this->capacity = rht.capacity;
+
+  if(this->table!=nullptr) {
+    delete[] this->table;
+  }
+  this->table = new string [this->capacity];
+  if(this->table == nullptr){
+    cerr << "memory allocation problem" << endl;
+    exit(-1);
+  }
+  for(int i=0; i<this->capacity;i++){
+    this->table[i] = rht.table[i];
+  }
+}
+
+bool ExtensibleHashTable::add(const string &str){
+  if(this->HashTable::add(str)){
+    this->rehash();
+    return true;
+  }else{
+    return false;
+  }
+}
+
+bool ExtensibleHashTable::remove(const string &str){
+  if(this->HashTable::remove(str)){
+    this->rehash();
+    return true;
+  }else{
+    return false;
+  }
+}
+
+bool ExtensibleHashTable::operator<<(string str){
+  return this->add(str);
+}
+
+bool ExtensibleHashTable::operator>>(string str){
+  return this->remove(str);
+}
+
+ExtensibleHashTable ExtensibleHashTable::operator+(const ExtensibleHashTable &t) const{
+  ExtensibleHashTable ht;
+
+  for(int i = 0;i<this->capacity;i++){
+    ht.add(this->table[i]);
+  }
+  ht.print();
+
+  cout << endl;
+
+  for(int i = 0;i<t.capacity;i++){
+    ht.add(t.table[i]);
+  }
+
+  ht.print();
+
+  return ht;
+}
+
+ExtensibleHashTable &ExtensibleHashTable::operator+=(const ExtensibleHashTable &t){
+  for(int i=0;i<t.capacity;i++){
+    this->add(t.table[i]);
+  }
+  return *this;
+}
+
+ExtensibleHashTable &ExtensibleHashTable::operator=(const ExtensibleHashTable &t){
+  this->capacity = t.capacity;
+  this->size = t.size;
+  delete []this->table;
+  this->table = new string[this->capacity];
+  if(this->table == nullptr){
+    cerr << "memory allocation problem" << endl;
+    exit(-1);
+  }
+  for(int i=0;i<t.capacity;i++)
+    this->table[i] = t.table[i];
+
+  return *this;
+}
+
+#endif
---
+++ Katsaros_Nikolaos_1648/HashTable.h
@@ -0,0 +1,293 @@
+
+#ifndef __HASHTABLE_H__
+#define __HASHTABLE_H__
+
+//#include "HashTableIterator.h"
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+class HashTable {
+
+protected:
+  unsigned int size;
+  unsigned int capacity;
+  string *table;
+  static int getHashCode(const char *str);
+  static bool checkDeadOrEmpty(const string &s);
+  static bool checkDeadOrEmpty(const char * s);
+
+public:
+  class Iterator;
+
+  HashTable(int size=8);
+  HashTable(const HashTable &ht);
+  int getSize();
+  int getCapacity();
+  bool isEmpty(int pos);
+  bool isTomb(int pos);
+  bool isAvailable(int pos);
+  bool contains(const string &s);
+  bool contains(const char *s);
+  virtual bool add(const string &s);
+  virtual bool add(const char *s);
+  virtual bool remove(const string &s);
+  virtual bool remove(const char *s);
+  Iterator begin() const;
+  Iterator end() const;
+  void print() ;
+
+  virtual bool operator << (string str);
+  virtual bool operator >> (string str);
+  HashTable operator+(const HashTable &ht);
+  HashTable &operator+=(const HashTable &t);
+  HashTable &operator=(const HashTable &t);
+
+  class Iterator {
+    string *p, *end;
+  public:
+    Iterator operator++();
+    Iterator operator--();
+    Iterator operator++(int a);
+    Iterator operator--(int a);
+    bool operator<(Iterator it);
+    string &operator*();
+    friend Iterator HashTable::begin() const;
+    friend Iterator HashTable::end() const;
+  };
+};
+
+HashTable::HashTable(int size){
+  this->size = 0;
+  this->capacity = size;
+  this->table = new string[capacity];
+  if(this->table == nullptr){
+    cerr << "memory allocation problem" << endl;
+    exit(-1);
+  }
+}
+
+HashTable::HashTable(const HashTable &ht){
+  this->size = ht.size;
+  this->capacity = ht.capacity;
+  this->table = new string[ht.capacity];
+  if(this->table == nullptr){
+    cerr << "memory allocation problem" << endl;
+    exit(-1);
+  }
+}
+
+void HashTable::print() {
+  for(int i=0;i<capacity;i++){
+    if(!isAvailable(i)){
+      cout << i << " : " << table[i]<< endl;
+    }
+  }
+  cout << "# capacity: " << capacity << "\n# size: " << size << endl;
+}
+
+int HashTable::getHashCode(const char *str){
+  int hash = 0;
+  for(int i=0;i<strlen(str);i++){
+    char c = str[i];
+    hash += int(c);
+  }
+  return(hash);
+}
+
+bool HashTable::checkDeadOrEmpty(const string &s){
+  if(s == "" || s == "##tomb##")
+    return true;
+  else
+    return false;
+}
+
+bool HashTable::checkDeadOrEmpty(const char *s){
+  if(s == "" || s == "##tomb##")
+    return true;
+  else
+    return false;
+}
+
+int HashTable::getSize(){
+  return(size);
+}
+
+int HashTable::getCapacity(){
+  return(capacity);
+}
+
+bool HashTable::isEmpty(int pos){
+  if(pos>= capacity){
+    return(false);
+  }else if(table[pos] == ""){
+    return(true);
+  }else{
+    return(false);
+  }
+}
+
+bool HashTable::isTomb(int pos){
+  if(pos>= capacity){
+    return(false);
+  }else if(table[pos] == "##tomb##"){
+    return(true);
+  }
+  return(false);
+}
+
+bool HashTable::isAvailable(int pos){
+  if(isEmpty(pos) || isTomb(pos)){
+    return (true);
+  }
+  return(false);
+}
+
+bool HashTable::contains(const string &s){
+	for(int i=0;i<capacity;i++){
+		if(s == table[i]){
+			return(true);
+		}
+	}
+	return(false);
+}
+
+bool HashTable::contains(const char *s){
+  for(int i=0;i<capacity;i++){
+		if(s == table[i]){
+      return(true);
+		}
+	}
+	return(false);
+}
+
+bool HashTable::add(const string & s){
+  int hash = getHashCode(s.c_str()) % capacity;
+  if(contains(s) || checkDeadOrEmpty(s)){
+    return false;
+  } else{
+    for (int i=0; i<capacity;i++){
+      if(isAvailable((hash+i)%capacity)){
+        table[(hash+i)%capacity] = s;
+        size++;
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+bool HashTable::add(const char * s){
+  int hash = getHashCode(s) % capacity;
+  if(contains(s) || checkDeadOrEmpty(s)){
+    return false;
+  } else{
+    for (int i=0; i<capacity;i++){
+      if(isAvailable((hash+i)%capacity)){
+        table[(hash+i)%capacity] = s;
+        size++;
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+bool HashTable::remove(const string & s){
+  if(!contains(s)){
+    return false;
+  } else{
+    for (int i=0; i<capacity;i++){
+      if(table[i] == s){
+        table[i] = "##tomb##";
+        size--;
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+bool HashTable::remove(const char * s){
+  if(!contains(s)){
+    return false;
+  } else{
+    for (int i=0; i<capacity;i++){
+      if(table[i] == s){
+        table[i] = "##tomb##";
+        size--;
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+bool HashTable::operator << (string str){
+  return add(str);
+}
+
+bool HashTable::operator >> (string str){
+  return remove(str);
+}
+
+HashTable HashTable::operator+(const HashTable &ht){
+  HashTable newHt(this->capacity + ht.capacity);
+  for(int i=0;i<capacity;i++){
+    newHt.add(table[i]);
+  }
+  for(int i=0;i<ht.capacity;i++){
+    newHt.add(ht.table[i]);
+  }
+
+  return newHt;
+}
+
+HashTable &HashTable::operator+=(const HashTable &t){
+  string *tableBuffer;
+  tableBuffer = new string[capacity];
+  if(tableBuffer == nullptr){
+    cerr << "memory allocation problem" << endl;
+    exit(-1);
+  }
+  for(int i=0;i<capacity;i++){
+    tableBuffer[i] = this->table[i];
+  }
+  delete []this->table;
+  this->capacity = capacity+t.capacity;
+  this->size = 0;
+  this->table = new string[capacity];
+  if(this->table == nullptr){
+    cerr << "memory allocation problem" << endl;
+    exit(-1);
+  }
+
+  for(int i=0;i<capacity-t.capacity;i++){
+    add(tableBuffer[i]);
+  }
+    cout << "here" << endl;
+
+  for(int i=0;i<t.capacity;i++){
+    add(t.table[i]);
+  }
+}
+
+HashTable &HashTable::operator=(const HashTable &t){
+  delete []this->table;
+  this->capacity = t.capacity;
+  this->size = 0;
+  this->table = new string[t.capacity];
+  if(this->table == nullptr){
+    cerr << "memory allocation problem" << endl;
+    exit(-1);
+  }
+  for(int i=0;i<this->capacity;i++){
+    add(t.table[i]);
+  }
+  return *this;
+}
+
+
+#endif
